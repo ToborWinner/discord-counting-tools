ee0753288ec08d95a763c3cbb948a76e518d7ffe
toborwinner <102221758+ToborWinner@users.noreply.github.com>
2024-09-24 21:28:45 +0200

    Added nix environment

---
+++ flake.nix
@@ -0,0 +1,35 @@
+{
+  description = "A Nix-flake-based Java development environment";
+
+  outputs = { self, nixpkgs }:
+    let
+      javaVersion = 22; # Change this value to update the whole stack
+
+      supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      forEachSupportedSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f {
+        pkgs = import nixpkgs { inherit system; overlays = [ self.overlays.default ]; };
+      });
+    in
+    {
+      overlays.default =
+        final: prev: rec {
+          jdk = prev."jdk${toString javaVersion}";
+          # maven = prev.maven.override { jdk_headless = jdk; };
+          # gradle = prev.gradle.override { java = jdk; };
+        };
+
+      devShells = forEachSupportedSystem ({ pkgs }: {
+        default = pkgs.mkShell {
+          packages = with pkgs; [
+            # gradle
+            jdk
+            # maven
+          ];
+        };
+      });
+
+      shellHook = ''
+        exec zsh
+      '';
+  };
+}
