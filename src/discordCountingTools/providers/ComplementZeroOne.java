c187034cd93e51fbf92aeb2e94d181883791637c
toborwinner <102221758+ToborWinner@users.noreply.github.com>
2024-10-19 15:08:38 +0200

    Bad fix to the 01 one

--- src/discordCountingTools/providers/ComplementZeroOne.java
+++ src/discordCountingTools/providers/ComplementZeroOne.java
@@ -5,6 +5,6 @@ import discordCountingTools.generators.SubtractPowersOfTwo;
 public class ComplementZeroOne extends SubtractPowersOfTwo {
 	@Override
 	protected String getPowerOfTwo(int n) {
-		return "1\\-(~~~0" + "\\-~~~1".repeat(n) + ")";
+		return "1/-(~~~0" + "/-~~~1".repeat(n) + ")";
 	}
 }
