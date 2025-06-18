2ff3aa1fe643cd20cbed98140339810366364e9c
toborwinner <102221758+ToborWinner@users.noreply.github.com>
2025-06-19 01:47:27 +0200

    removed unsupported modes from counting classic mode

--- src/discordCountingTools/Main.java
+++ src/discordCountingTools/Main.java
@@ -51,7 +51,7 @@ public class Main {
 		if (mode.equals("rand")) {
 			gen = getRandomGenerator(generators, num, null);
 		} else if (mode.equals("classic")) {
-			gen = getRandomGenerator(generators, num, "x v e l rs");
+			gen = getRandomGenerator(generators, num, "x v e l rs inf eexp");
 		} else {
 			gen = getGeneratorFromKey(generators, mode);
 		}
